a627cf5fde2ab4bd6158113e703639db3b5b6791
Simon Yuen <simonyuen1999@hotmail.com>
2023-12-27 11:31:59 -0500

    Initial checkin

--- ezapp2/ezapp2_stack.py
+++ ezapp2/ezapp2_stack.py
@@ -2,6 +2,9 @@ from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_s3 as s3,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
 )
 from constructs import Construct
 
@@ -11,6 +14,21 @@ class Ezapp2Stack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        # Create a new S3 bucket
+        self.bucket = s3.Bucket( self, "ezApp2Bucket", versioned=True)
+
+        # Create a new Lambda function
+        self.lambda_function = _lambda.Function(
+            self,                                     # Parent   
+            "ezApp2Lambda",                          # The name of the lambda
+            code=_lambda.Code.from_asset("lambda"),  # Specify the lambda directory,
+            handler="handler.handler",               # the handler file and the handler function
+            runtime=_lambda.Runtime.PYTHON_3_9,
+        )
+
+        # Create a new API Gateway
+        self.api_gateway = apigw.RestApi(self, "ezApp2Api")
+        self.api_gateway.root.add_method("GET", apigw.LambdaIntegration(self.lambda_function))
 
         # example resource
         # queue = sqs.Queue(
